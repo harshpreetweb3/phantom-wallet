045b447b47efe7886a9608467df31c5d5f6dcd80
harshpreetweb3 <harshpreetweb3@gmail.com>
2024-10-25 14:17:49 +0530

    meta-mask integration done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-// import logo from './logo.svg';
 import './App.css';
 import getProvider from './utils/getProvider';
 import { useState, useEffect } from 'react';
+import { useSDK } from "@metamask/sdk-react";
 
 function App() {
-  const [pubKey, setPublicKey] = useState();
 
-  const provider = getProvider();
+  const [account, setAccount] = useState("");
+  const { sdk, connected, connecting, provider, chainId } = useSDK();
+
+  const connect = async () => {
+    try {
+      const accounts = await sdk?.connect();
+      setAccount(accounts?.[0]);
+    } catch (err) {
+      console.warn("failed to connect..", err);
+    }
+  };
+
+  const [pubKey, setPublicKey] = useState();
+  const phantom_provider = getProvider();
 
   useEffect(() => {
     // Check if the user has explicitly disconnected
@@ -14,7 +26,7 @@ function App() {
 
     if (!isDisconnected) {
       // Attempt to eagerly connect to Phantom
-      provider.connect({ onlyIfTrusted: true })
+      phantom_provider.connect({ onlyIfTrusted: true })
         .then(({ publicKey }) => {
           // Handle successful eager connection
           console.log(publicKey.toString());
@@ -27,34 +39,34 @@ function App() {
     }
 
     // Set up event listener for account changes
-    provider.on('accountChanged', async (publicKey) => {
+    phantom_provider.on('accountChanged', async (publicKey) => {
       if (publicKey) {
         // Set new public key and continue as usual
         console.log(`Switched to account ${publicKey.toBase58()}`);
         setPublicKey(publicKey.toString());
       } else {
         // Attempt to reconnect to Phantom
-        await provider.connect().catch((error) => {
+        await phantom_provider.connect().catch((error) => {
           // Handle connection failure
           console.log("Didn't show changed account so we will try to reconnect");
         });
 
-        setPublicKey(provider.publicKey.toString());
+        setPublicKey(phantom_provider.publicKey.toString());
       }
     });
 
     // Cleanup event listener on component unmount
     return () => {
-      provider.removeAllListeners('accountChanged');
+      phantom_provider.removeAllListeners('accountChanged');
     };
-  }, [provider]);
+  }, [phantom_provider]);
 
   async function handleConnect() {
     try {
-      const response = await provider.connect();
+      const response = await phantom_provider.connect();
       console.log(response.publicKey.toString());
       setPublicKey(response.publicKey.toString());
-      provider.on("connect", () => console.log("connected!"));
+      phantom_provider.on("connect", () => console.log("connected!"));
 
       // Clear the disconnected state
       localStorage.setItem('isDisconnected', 'false');
@@ -64,7 +76,7 @@ function App() {
   }
 
   async function handleDisconnect() {
-    await provider.disconnect();
+    await phantom_provider.disconnect();
     console.log("Phantom wallet disconnected");
     setPublicKey(null);
 
@@ -72,7 +84,7 @@ function App() {
     localStorage.setItem('isDisconnected', 'true');
 
     // Forget user's public key once they disconnect
-    provider.on("disconnect", () => {
+    phantom_provider.on("disconnect", () => {
       console.log("disconnected!");
     });
   }
@@ -87,9 +99,26 @@ function App() {
           : <button onClick={() => { handleDisconnect() }}>disconnect phantom wallet</button>
         }
 
-        {/* <p>wallet connection status : {provider.isConnected}</p> */}
+        {/* <p>wallet connection status : {phantom_provider.isConnected}</p> */}
 
         {pubKey ? <p> Wallet Connected : your public key is : {pubKey} </p> : <p>Wallet Not Connected</p>}
+
+        {/* metamask wallet integration */}
+
+        <button style={{ padding: 10, margin: 10 }} onClick={connect}>
+          Connect
+        </button>
+
+        {connected && (
+        <div>
+          <>
+            {chainId && `Connected chain: ${chainId}`}
+            <p></p>
+            {account && `Connected account: ${account}`}
+          </>
+        </div>
+      )}
+
       </header>
     </div>
   );
